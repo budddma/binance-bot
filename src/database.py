be8addf98f040d5bf091d9d85fd42a6457dd7cc8
Марат Будажапов <98969139+budddma@users.noreply.github.com>
2023-05-15 00:35:43 +0300

    Update database.py

--- src/database.py
+++ src/database.py
@@ -2,49 +2,47 @@ import sqlite3 as sq
 
 
 def create_table():
-    with sq.connect("users.db") as conn:
-        conn.execute(
-            "CREATE TABLE IF NOT EXISTS user_data (username VARCHAR(30), pair VARCHAR(10), timeframe VARCHAR(5), indicators TEXT)"
-        )
+    global conn, cur
+    conn = sq.connect("users.db")
+    cur = conn.cursor()
 
-
-async def insert_into_table(state):
-    with sq.connect("users.db") as conn:
-        cur = conn.cursor()
-
-        async with state.proxy() as data:
-            if data["username"] in get_usernames():
-                cur.execute(
-                    "UPDATE user_data SET pair=?, timeframe=?, indicators=? WHERE username=?",
-                    (
-                        data["pair"],
-                        data["timeframe"],
-                        data["indicators"],
-                        data["username"],
-                    ),
-                )
-
-            else:
-                cur.execute(
-                    "INSERT INTO user_data VALUES (?, ?, ?, ?)", tuple(data.values())
-                )
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS user_data (username VARCHAR(30), pair VARCHAR(10), timeframe VARCHAR(5), indicators TEXT)"
+    )
+    conn.commit()
 
 
 def get_usernames():
-    with sq.connect("users.db") as conn:
-        cur = conn.cursor()
+    tuples = cur.execute("SELECT username FROM user_data").fetchall()
+    usernames = [str(tpl[0]) for tpl in tuples]
+    conn.commit()
+    return usernames
 
-        tuples = cur.execute("SELECT username FROM user_data").fetchall()
-        usernames = [str(tpl[0]) for tpl in tuples]
-        return usernames
+
+async def insert_into_table(state):
+    async with state.proxy() as data:
+        if data["username"] in get_usernames():
+            cur.execute(
+                "UPDATE user_data SET pair=?, timeframe=?, indicators=? WHERE username=?",
+                (
+                    data["pair"],
+                    data["timeframe"],
+                    data["indicators"],
+                    data["username"],
+                ),
+            )
+
+        else:
+            cur.execute(
+                "INSERT INTO user_data VALUES (?, ?, ?, ?)", tuple(data.values())
+            )
+    conn.commit()
 
 
 def get_user_data(username):
-    with sq.connect("users.db") as conn:
-        cur = conn.cursor()
-
-        user_data = cur.execute(
-            "SELECT pair, timeframe, indicators FROM user_data WHERE username=?",
-            (username,),
-        ).fetchone()
-        return user_data
+    user_data = cur.execute(
+        "SELECT pair, timeframe, indicators FROM user_data WHERE username=?",
+        (username,),
+    ).fetchone()
+    conn.commit()
+    return user_data
